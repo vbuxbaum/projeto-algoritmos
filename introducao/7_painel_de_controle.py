0240fc94a487fb6bf025e6ff3be943e67c3818ab
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-02-19 12:07:21 -0300

    implementação 1.7

---
+++ introducao/7_painel_de_controle.py
@@ -0,0 +1,12 @@
+class PainelDeControle:
+    def __init__(self, temp_atual=0, temp_desejada=0):
+        self.__temperatura_atual = temp_atual
+        self.__temperatura_desejada = temp_desejada
+
+    def ligar_forno(self):
+        # Código do método
+        pass
+
+    def desligar_forno(self):
+        # Código do método
+        pass
