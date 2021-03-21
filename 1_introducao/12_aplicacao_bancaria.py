ad01fa4c7cfc2a068966a45532a03ab49a48e36b
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-03-21 18:25:23 -0300

    1.12 - Decisão de implementação + assert

--- 1_introducao/12_aplicacao_bancaria.py
+++ 1_introducao/12_aplicacao_bancaria.py
@@ -1,3 +1,10 @@
+"""
+Nesta implementação em Java, o método imprime() lança Strings na saída padrão.
+Para facilitar a implementação de testes, o comportamento desse método foi
+alterado para que apenas retorne as respectivas Strings.
+"""
+
+
 class ContaBancaria:
     def __init__(self, saldo_inicial):
         self.saldo = saldo_inicial
@@ -12,9 +19,11 @@ class ContaBancaria:
         return "Saldo = $" + str(self.saldo)
 
 
-conta_teste = ContaBancaria(100)
+conta_teste = ContaBancaria(200.00)
 
-conta_teste.depositar(30)
-conta_teste.sacar(20)
+assert conta_teste.verifica_saldo() == "Saldo = $200.0"
 
-assert conta_teste.verifica_saldo() == "Saldo = $110"
+conta_teste.depositar(50.00)
+conta_teste.sacar(70.00)
+
+assert conta_teste.verifica_saldo() == "Saldo = $180.0"
