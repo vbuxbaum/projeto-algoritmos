4d2f7ecf1d141fc7c8bd996080ecf7b346ef042a
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-05-04 11:37:39 -0300

    bux piloto

---
+++ cap5/arvore.py
@@ -0,0 +1,16 @@
+''' Implementação de Arvore Binária com Programação Orientada a Objetos '''
+
+class ArvoreBinaria:
+    '''Uma Árvore Binária é uma estrutura de dados composta de nós com 2 
+    filhos conectados unidirecionalmente'''
+    def __init__(self):
+        self.no_raiz = None
+        self.no_esquerdo = None
+        self.no_direito = None
+
+
+
+
+if __name__ == "__main__":
+    conjunto = [1, 2, 3, 4, 5, 6, 10, 54, -1, -67]
+    assert True
