fd83a4bf61fd5c377ee6529b522368fb947201d6
NicolasFirmo <nicolasfofo@hotmail.com>
2021-05-04 11:55:43 -0300

    Piloto Nicolas e navegador Gleison

--- cap5/arvore.py
+++ cap5/arvore.py
@@ -1,6 +1,9 @@
 ''' Implementação de Arvore Binária com Programação Orientada a Objetos '''
+
+
 class No:
     '''Um Nó de Árvore binária tem duas ligações.'''
+
     def __init__(self, valor):
         self.valor = valor
         self.no_esquerdo = None
@@ -10,10 +13,20 @@ class No:
 class ArvoreBinaria:
     '''Uma Árvore Binária é uma estrutura de dados composta de nós com 2 
     filhos conectados unidirecionalmente'''
+
     def __init__(self):
         self.no_raiz = None
 
+    def insere(self, no):
+        no_atual = self.no_raiz
 
+        while no_atual:
+            if no < no_atual:
+                no_atual = no_atual.no_esquerdo
+            else:
+                no_atual = no_atual.no_direito
+
+        no_atual = no
 
 
 if __name__ == "__main__":
